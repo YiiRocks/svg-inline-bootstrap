53dd3cc1765a15a4ac38d8affb7e03c4f255ae3b
Mr.42 <johan@mr42.me>
2020-11-26 22:43:39 +0100

    Cleanup tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,10 +8,8 @@ use Psr\Container\ContainerInterface;
 use YiiRocks\SvgInline\SvgInline;
 use YiiRocks\SvgInline\SvgInlineInterface;
 use Yiisoft\Aliases\Aliases;
-use Yiisoft\Assets\AssetManager;
 use Yiisoft\Composer\Config\Builder;
 use Yiisoft\Di\Container;
-use Yiisoft\Files\FileHelper;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
@@ -19,11 +17,6 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
      * @var Aliases $aliases
      */
     protected $aliases;
-
-    /**
-     * @var AssetManager $assetManager
-     */
-    protected $assetManager;
 
     /**
      * @var SvgInline $svgInline
@@ -46,32 +39,5 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
         $this->aliases->set('@assetsUrl', '/baseUrl');
         $this->aliases->set('@vendor', '@root/vendor');
         $this->svgInline = $this->container->get(SvgInlineInterface::class);
-    }
-
-    protected function tearDown(): void
-    {
-        $this->container = null;
-        $this->removeAssets('@assets');
-        parent::tearDown();
-    }
-
-    protected function removeAssets(string $basePath): void
-    {
-        $handle = opendir($dir = $this->aliases->get($basePath));
-        if ($handle === false) {
-            throw new \Exception("Unable to open directory: $dir");
-        }
-        while (($file = readdir($handle)) !== false) {
-            if ($file === '.' || $file === '..' || $file === '.gitignore') {
-                continue;
-            }
-            $path = $dir . DIRECTORY_SEPARATOR . $file;
-            if (is_dir($path)) {
-                FileHelper::removeDirectory($path);
-            } else {
-                FileHelper::unlink($path);
-            }
-        }
-        closedir($handle);
     }
 }
